f7e2556c278f76011a6a9033ea4d4350f2f8b6c5
ViragDora88 <virag.kerecz@gmail.com>
2025-04-06 14:19:23 +0200

    Adatbázis

---
+++ login (1).sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Ápr 06. 14:18
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kepek`
+--
+
+CREATE TABLE `kepek` (
+  `Id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `image` multipoint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `megrendeles`
+--
+
+CREATE TABLE `megrendeles` (
+  `Id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `kepid` int(11) NOT NULL,
+  `kepek` multipoint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `nev` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `kepekfeltoltes` int(11) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
+  `lockUser` int(11) NOT NULL,
+  `deleteUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`Id`, `nev`, `email`, `username`, `password`, `kepekfeltoltes`, `is_locked`, `lockUser`, `deleteUser`) VALUES
+(1, 'Felhasználó', '', 'user', 'password', 0, 0, 0, 0),
+(8, 'baba', 'baba@baba.hu', 'Dolly', '$2y$10$qAliXwXP096c8.oQ8scCwes6tGPXJSmosanolp/JfbW', 0, 0, 0, 0);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
